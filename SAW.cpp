b2d2330652ba04d8c69aed9c6f7c789fe2333c15
AKaspulAnwar <32286210+AKaspulAnwar@users.noreply.github.com>
2018-01-03 22:10:36 +0800

    error lagi

--- SAW.cpp
+++ SAW.cpp
@@ -24,13 +24,14 @@ int main(){
 		mama3++;
 	}while(mama3<5);cout<<endl;
 	//cukup dulu
-	double mc1=max(z1y[0],max(z2y[0],z3y[0)),
-	mc2=max(z1y[1],max(z2y[1],z3y[0])),
-	mc3=max(z1y[2],max(z2y[2],z3y[0])),
-	mc4=max(z1y[3],max(z2y[3],z3y[0]));
+	double mc1=max(z1y[0],max(z2y[0],z3y[0])),
+	mc2=max(z1y[1],max(z2y[1],z3y[1])),
+	mc3=max(z1y[2],max(z2y[2],z3y[2])),
+	mc4=max(z1y[3],max(z2y[3],z3y[3])),
+	mc5=max(z1y[4],max(z2y[4],z3y[4]));
 	
-	double Rz1y[4]={ (z1y[0]/max1),(z1y[1]/max2),(z1y[2]/max3),(z1y[3]/max4),(z1y[4]/max5)};
-	double Rz2y[4]={ (z2y[0]/max1),(z2y[1]/max2),(z2y[2]/max3),(z2y[3]/max4),(z1y[4]/max5)};
-	double Rz3y[4]={ (z3y[0]/max1),(z3y[1]/max2),(z3y[2]/max3),(z3y[3]/max4),(z1y[4]/max5)};
+	double Rz1y[5]={ (z1y[0]/mc1),(z1y[1]/mc2),(z1y[2]/mc3),(z1y[3]/mc4),(z1y[4]/mc5) };
+	double Rz2y[5]={ (z2y[0]/mc1),(z2y[1]/mc2),(z2y[2]/mc3),(z2y[3]/mc4),(z2y[4]/mc5) };
+	double Rz3y[5]={ (z3y[0]/mc1),(z3y[1]/mc2),(z3y[2]/mc3),(z3y[3]/mc4),(z3y[4]/mc5) };
 	//r
 }
