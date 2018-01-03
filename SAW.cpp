b9c8b147809e1bd013d3edc2458c3741c8d9693e
AKaspulAnwar <32286210+AKaspulAnwar@users.noreply.github.com>
2018-01-03 21:30:11 +0800

    masukan gan

--- SAW.cpp
+++ SAW.cpp
@@ -5,5 +5,6 @@ using namespace std;
 int main(){
 	cout.setf(ios::fixed); cout.setf(ios::showpoint); cout.precision(4);
 	//test gan
-	
+	double z1y[4]={4,4,3,3}, z2y[4]={3,3,4,3}, z3y[4]={4,2,2,2};
+	//masukan gan
 }
