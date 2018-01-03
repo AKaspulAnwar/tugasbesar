143fab293de32c07870f9159a0cf95dd7d870ef8
AKaspulAnwar <32286210+AKaspulAnwar@users.noreply.github.com>
2018-01-03 22:39:43 +0800

    banyak salah

--- SAW.cpp
+++ SAW.cpp
@@ -30,51 +30,51 @@ int main(){
 	mc4=max(z1y[3],max(z2y[3],z3y[3])),
 	mc5=max(z1y[4],max(z2y[4],z3y[4]));
 	
-	double Rz1y[5]={ (z1y[0]/mc1),(z1y[1]/mc2),(z1y[2]/mc3),(z1y[3]/mc4),(z1y[4]/mc5) };
-	double Rz2y[5]={ (z2y[0]/mc1),(z2y[1]/mc2),(z2y[2]/mc3),(z2y[3]/mc4),(z2y[4]/mc5) };
-	double Rz3y[5]={ (z3y[0]/mc1),(z3y[1]/mc2),(z3y[2]/mc3),(z3y[3]/mc4),(z3y[4]/mc5) };
+	double Az1y[5]={ (z1y[0]/mc1),(z1y[1]/mc2),(z1y[2]/mc3),(z1y[3]/mc4),(z1y[4]/mc5) };
+	double Az2y[5]={ (z2y[0]/mc1),(z2y[1]/mc2),(z2y[2]/mc3),(z2y[3]/mc4),(z2y[4]/mc5) };
+	double Az3y[5]={ (z3y[0]/mc1),(z3y[1]/mc2),(z3y[2]/mc3),(z3y[3]/mc4),(z3y[4]/mc5) };
 	//rooor
 	cout << endl<<endl;
 	cout << "MASTRIS B :"<<endl;
 	int abah1=0;
 	do{
-		cout << Rz1y[abah1]<<" | ";
+		cout << Az1y[abah1]<<" | ";
 		abah1++;
 	}while(abah1<5);cout<<endl;
 	int abah2=0;
 	do{
-		cout << Rz2y[abah2]<<" | ";
+		cout << Az2y[abah2]<<" | ";
 		abah2++;
 	}while(abah2<5);cout<<endl;
 	int abah3=0;
 	do{
-		cout << Rz3y[abah3]<<" | ";
+		cout << Az3y[abah3]<<" | ";
 		abah3++;
 	}while(abah3<5);cout<<endl;
 	//the last
-	double w1=5, w2=3, w3=4, w4=4, w5=2;
-	cout << endl << "jadinya kayapa:"<<endl<< w1<<" | "<< w2<<" | "<< w3<<" | "<< w4<<" | "<< w5<<" | "<<endl;
+	double B1=5, B2=3, B3=4, B4=4, B5=2;
+	cout << endl << "jadinya kayapa:"<<endl<< B1<<" | "<< B2<<" | "<< B3<<" | "<< B4<<" | "<< B5<<" | "<<endl;
 	//r*w
-	double RWz1y[5]={ (Rz1y[0]*w1),(Rz1y[1]*w2),(Rz1y[2]*w3),(Rz1y[3]*w4),(Rz1y[4]*w5) };
-	double RWz2y[5]={ (Rz2y[0]*w1),(Rz2y[1]*w2),(Rz2y[2]*w3),(Rz2y[3]*w4),(Rz2y[4]*w5) };
-	double RWz3y[5]={ (Rz3y[0]*w1),(Rz3y[1]*w2),(Rz3y[2]*w3),(Rz3y[3]*w4),(Rz3y[4]*w5) };
+	double ABz1y[5]={ (Az1y[0]*B1),(Az1y[1]*B2),(Az1y[2]*B3),(Az1y[3]*B4),(Az1y[4]*B5) };
+	double ABz2y[5]={ (Az2y[0]*B1),(Az2y[1]*B2),(Az2y[2]*B3),(Az2y[3]*B4),(Az2y[4]*B5) };
+	double ABz3y[5]={ (Az3y[0]*B1),(Az3y[1]*B2),(Az3y[2]*B3),(Az3y[3]*B4),(Az3y[4]*B5) };
 	//r*w
 	cout << endl<<endl;
 	cout << "MASTRIS A * B :"<<endl;
 	int anak1=0;
 	do{
-		cout << RWz1y[anak1]<<" | ";
+		cout << ABz1y[anak1]<<" | ";
 		anak1++;
 	}while(anak1<5);cout<<endl;
 	
 	int anak2=0;
 	do{
-		cout << RWz2y[anak2]<<" | ";
+		cout << ABz2y[anak2]<<" | ";
 		anak2++;
 	}while(anak2<5);cout<<endl;
 	int anak3=0;
 	do{
-		cout << RWz3y[anak3]<<" | ";
+		cout << ABz3y[anak3]<<" | ";
 		anak3++;
 	}while(anak3<5);cout<<endl;
 	//akhirnya
